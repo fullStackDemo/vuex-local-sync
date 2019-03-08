ded299a259f2166f0e149549479a628522382b7c
wangzhao <wangzhao@go-goal.com>
2019-03-08 15:33:49 +0800

    initializing project

---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+import { Store } from 'vuex';
+export default Store;
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+import {Mutation, MutationPayload, Payload, Plugin, Store} from 'vuex';
+
+console.log(Store.prototype);
+
+
+
+export default Store;
